a8fc82fbe53df0c3978b26c5c0d60ff732100b24
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2022-08-17 22:04:57 +0200

    feat: add .env and api.ts

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -10,50 +10,43 @@ export const Header = () => {
   const { pathname } = useRouter()
 
   return (
-    <header className='position-fixed w-100'>
-      <div className='px-3 py-2 text-bg-dark bg-dark'>
-        <Container>
-          <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start'>
-            <Link href={INDEX_PATH}>
-              <a className='h2 text-white text-decoration-none d-none me-5 d-lg-block'>GoPOS</a>
-            </Link>
-            <form className='me-lg-auto col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3' role='search'>
-              <input
-                type='search'
-                className='form-control text-white bg-dark'
-                placeholder='Search...'
-                aria-label='Search'
-              />
-            </form>
-            <ul className='nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small'>
-              <li className={clsx('btn btn-dark', pathname === INDEX_PATH && 'active')}>
-                <Link href={INDEX_PATH}>
-                  <a className='nav-link text-white'>
-                    <BiHome style={{ width: 24, height: 24 }} className='bi d-block mx-auto mb-1' />
-                    Home
-                  </a>
-                </Link>
-              </li>
-              <li className={clsx('btn btn-dark', pathname === PRODUCTS_PATH && 'active')}>
-                <Link href={PRODUCTS_PATH}>
-                  <a className='nav-link text-white'>
-                    <BiCategory style={{ width: 24, height: 24 }} className='bi d-block mx-auto mb-1' />
-                    Products
-                  </a>
-                </Link>
-              </li>
-              <li className={clsx('btn btn-dark', pathname === CATEGORIES_PATH && 'active')}>
-                <Link href={CATEGORIES_PATH}>
-                  <a className='nav-link text-white'>
-                    <BiCollection style={{ width: 24, height: 24 }} className='bi d-block mx-auto mb-1' />
-                    Categories
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </Container>
-      </div>
+    <header className='position-fixed w-100 px-3 py-2 text-bg-dark bg-dark'>
+      <Container>
+        <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start'>
+          <Link href={INDEX_PATH}>
+            <a className='h2 text-white text-decoration-none d-none me-5 d-lg-block'>GoPOS</a>
+          </Link>
+          <form className='mx-auto col-xl-6 me-lg-auto col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3' role='search'>
+            <input className='form-control text-white bg-dark' placeholder='Search...' aria-label='Search' />
+          </form>
+          <ul className='nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small'>
+            <li className={clsx('btn btn-dark', pathname === INDEX_PATH && 'active')}>
+              <Link href={INDEX_PATH}>
+                <a className='nav-link text-white'>
+                  <BiHome style={{ width: 24, height: 24 }} className='bi d-block mx-auto mb-1' />
+                  Home
+                </a>
+              </Link>
+            </li>
+            <li className={clsx('btn btn-dark', pathname === PRODUCTS_PATH && 'active')}>
+              <Link href={PRODUCTS_PATH}>
+                <a className='nav-link text-white'>
+                  <BiCategory style={{ width: 24, height: 24 }} className='bi d-block mx-auto mb-1' />
+                  Products
+                </a>
+              </Link>
+            </li>
+            <li className={clsx('btn btn-dark', pathname === CATEGORIES_PATH && 'active')}>
+              <Link href={CATEGORIES_PATH}>
+                <a className='nav-link text-white'>
+                  <BiCollection style={{ width: 24, height: 24 }} className='bi d-block mx-auto mb-1' />
+                  Categories
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Container>
     </header>
   )
 }
--- components/page-contents/HomePageContent.tsx
+++ components/page-contents/HomePageContent.tsx
@@ -1,6 +1,21 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
+
+import { API_URL } from '../../constants/api'
 
 export const HomePageContent = () => {
+  // eslint-disable-next-line -- it exists
+  const authorization = process.env.NEXT_PUBLIC_AUTHORIZATION!
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/219/product_categories`, {
+      mode: 'no-cors',
+      headers: {
+        Authorization: authorization
+      }
+    }).catch(err => console.log(err))
+  }, [authorization])
+
   return (
     <div className='d-flex flex-column align-items-center justify-content-center min-vh-100'>
       <Head>
